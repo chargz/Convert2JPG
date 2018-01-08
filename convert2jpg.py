ff349c429d5e1116c59005fa65f6e950bd4c3eca
abishek87 <abishek@scapic.com>
2018-01-08 23:44:57 +0530

    Minor fixes

--- convert2jpg.py
+++ convert2jpg.py
@@ -1,5 +1,4 @@
 #!/bin/python
-
 from PIL import Image
 import glob
 import os
@@ -7,21 +6,18 @@ import os
 path = raw_input('Enter your path:')
 bb = str(path)+"/*.png"
 filenamewpath = glob.glob(bb)
-
 leng = len(filenamewpath)
 
-if len(filenamewpath) <= 0:
-
+if leng = 0:
 		print "Did you get the path right? Looks like no PNG was found."
 
-for i in range(0, len(filenamewpath)):
-	filename = filenamewpath[i].split("/")[-1]
+elif leng >= 1:
+	for i in range(0, leng):
+		filename = filenamewpath[i].split("/")[-1]
 
-	im = Image.open(str(filenamewpath[i]))
-	rgb_im = im.convert('RGB')
-	rgb_im.save(str(path)+"/"+str(filename.split(".")[0])+'.jpg')
-
-if leng >= 1:	
+		im = Image.open(str(filenamewpath[i]))
+		rgb_im = im.convert('RGB')
+		rgb_im.save(str(path)+"/"+str(filename.split(".")[0])+'.jpg')
 	print str(leng)+" image(s) converted to JPG."
 
 # 	os.popen("rm *.png")
